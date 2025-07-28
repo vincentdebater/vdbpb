7d1122082d9e4c6852c17e10566d6df62edf2b28
vincent debater <wql.postbox@gmail.com>
2025-07-28 21:42:49 +0800

    Add files via upload

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -1,43 +1,38 @@
 import { SignJWT, jwtVerify } from 'jose';
-import nacl from 'tweetnacl';
-import { initializeParams, userID, origin } from "../helpers/init";
-import { renderLoginPage } from '../pages/login';
-import { renderErrorPage } from '../pages/error';
+import { randomBytes } from 'tweetnacl';
+import { respond } from '../helpers/helpers';
 
-async function generateJWTToken (request, env) {
-    await initializeParams(request, env);
+export async function generateJWTToken(request, env) {
+    if (request.method !== 'POST') return await respond(false, 405, 'Method not allowed.');
     const password = await request.text();
-    const savedPass = await env.bpb.get('pwd');
-    if (password !== savedPass) return new Response('Method Not Allowed', { status: 405 });
-    let secretKey = await env.bpb.get('secretKey');
+    const savedPass = await env.kv.get('pwd');
+    if (password !== savedPass) return await respond(false, 401, 'Wrong password.');
+    let secretKey = await env.kv.get('secretKey');
     if (!secretKey) {
         secretKey = generateSecretKey();
-        await env.bpb.put('secretKey', secretKey);
+        await env.kv.put('secretKey', secretKey);
     }
     const secret = new TextEncoder().encode(secretKey);
-    const jwtToken = await new SignJWT({ userID })
+    const jwtToken = await new SignJWT({ userID: globalThis.userID })
         .setProtectedHeader({ alg: 'HS256' })
         .setIssuedAt()
         .setExpirationTime('24h')
         .sign(secret);
 
-    return new Response('Success', {
-        status: 200,
-        headers: {
-            'Set-Cookie': `jwtToken=${jwtToken}; HttpOnly; Secure; Max-Age=${7 * 24 * 60 * 60}; Path=/; SameSite=Strict`,
-            'Content-Type': 'text/plain',
-        }
+    return await respond(true, 200, 'Successfully generated Auth token', null, {
+        'Set-Cookie': `jwtToken=${jwtToken}; HttpOnly; Secure; Max-Age=${7 * 24 * 60 * 60}; Path=/; SameSite=Strict`,
+        'Content-Type': 'text/plain',
     });
 }
 
-function generateSecretKey () {
-    const key = nacl.randomBytes(32);
+function generateSecretKey() {
+    const key = randomBytes(32);
     return Array.from(key, byte => byte.toString(16).padStart(2, '0')).join('');
 }
-  
-export async function Authenticate (request, env) {
+
+export async function Authenticate(request, env) {
     try {
-        const secretKey = await env.bpb.get('secretKey');
+        const secretKey = await env.kv.get('secretKey');
         const secret = new TextEncoder().encode(secretKey);
         const cookie = request.headers.get('Cookie')?.match(/(^|;\s*)jwtToken=([^;]*)/);
         const token = cookie ? cookie[2] : null;
@@ -56,37 +51,22 @@ export async function Authenticate (request, env) {
     }
 }
 
-export function logout() {
-    return new Response('Success', {
-        status: 200,
-        headers: {
-            'Set-Cookie': 'jwtToken=; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
-            'Content-Type': 'text/plain'
-        }
+export async function logout() {
+    return await respond(true, 200, 'Successfully logged out!', null, {
+        'Set-Cookie': 'jwtToken=; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+        'Content-Type': 'text/plain'
     });
 }
 
 export async function resetPassword(request, env) {
     let auth = await Authenticate(request, env);
-    const oldPwd = await env.bpb.get('pwd');
-    if (oldPwd && !auth) return new Response('Unauthorized!', { status: 401 });           
+    const oldPwd = await env.kv.get('pwd');
+    if (oldPwd && !auth) return await respond(false, 401, 'Unauthorized.');
     const newPwd = await request.text();
-    if (newPwd === oldPwd) return new Response('Please enter a new Password!', { status: 400 });
-    await env.bpb.put('pwd', newPwd);
-    return new Response('Success', {
-        status: 200,
-        headers: {
-            'Set-Cookie': 'jwtToken=; Path=/; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
-            'Content-Type': 'text/plain',
-        }
+    if (newPwd === oldPwd) return await respond(false, 400, 'Please enter a new Password.');
+    await env.kv.put('pwd', newPwd);
+    return await respond(true, 200, 'Successfully logged in!', null, {
+        'Set-Cookie': 'jwtToken=; Path=/; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+        'Content-Type': 'text/plain',
     });
-}
-
-export async function login(request, env) {
-    await initializeParams(request, env);
-    if (typeof env.bpb !== 'object') return await renderErrorPage(request, env, 'KV Dataset is not properly set!', null, true);
-    const auth = await Authenticate(request, env);
-    if (auth) return Response.redirect(`${origin}/panel`, 302);
-    if (request.method === 'POST') return await generateJWTToken(request, env);
-    return await renderLoginPage(request, env);
 }
