893db89d265034ff453bf10db9c3082e2ddde223
vincent debater <wql.postbox@gmail.com>
2025-07-28 21:44:22 +0800

    Add files via upload

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -1,28 +1,25 @@
-import { isValidUUID } from './helpers';
-const defaultProxyIP = 'bpb.yousef.isegaro.com';
-let userID, dohURL, proxyIP, trojanPassword, defaultHttpPorts, defaultHttpsPorts, panelVersion, hostName, origin, client, pathName;
-
-function initParams(request, env) {
-    const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
-    userID = env.UUID || '89b3cbba-e6ac-485a-9481-976a0415eab9';
-    if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
-    dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
-    proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : defaultProxyIP;
-    trojanPassword = env.TROJAN_PASS || 'bpb-trojan';
-    defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
-    defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
-    panelVersion = '2.7.6';
-    hostName = request.headers.get('Host');
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.search);
-    client = searchParams.get('app');
-    origin = url.origin;
-    pathName = url.pathname;
-}
+import { isValidUUID } from "./helpers";
+import pkg from '../../package.json' with { type: 'json' };
 
 export function initializeParams(request, env) {
-    initParams(request, env);
-    return Promise.resolve();
+    const url = new URL(request.url);
+    const searchParams = new URLSearchParams(url.search);
+    globalThis.panelVersion = pkg.version;
+    globalThis.defaultHttpPorts = [80, 8080, 2052, 2082, 2086, 2095, 8880];
+    globalThis.defaultHttpsPorts = [443, 8443, 2053, 2083, 2087, 2096];
+    globalThis.userID = env.UUID;
+    globalThis.TRPassword = env.TR_PASS;
+    globalThis.proxyIPs = env.PROXY_IP || 'bpb.yousef.isegaro.com';
+    globalThis.hostName = request.headers.get('Host');
+    globalThis.pathName = url.pathname;
+    globalThis.client = searchParams.get('app');
+    globalThis.urlOrigin = url.origin;
+    globalThis.dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
+    globalThis.fallbackDomain = env.FALLBACK || 'speed.cloudflare.com';
+    globalThis.subPath = env.SUB_PATH || globalThis.userID;
+    if (!['/error', '/secrets', '/favicon.ico'].includes(globalThis.pathName)) {
+        if (!globalThis.userID || !globalThis.TRPassword) throw new Error(`Please set UUID and Trojan password first. Please visit <a href="${globalThis.urlOrigin}/secrets" target="_blank">here</a> to generate them.`, { cause: "init" });
+        if (globalThis.userID && !isValidUUID(globalThis.userID)) throw new Error(`Invalid UUID: ${globalThis.userID}`, { cause: "init" });
+        if (typeof env.kv !== 'object') throw new Error('KV Dataset is not properly set! Please refer to tutorials.', { cause: "init" });
+    }
 }
-
-export { userID, dohURL, proxyIP, trojanPassword, hostName, origin, client, pathName, defaultHttpPorts, defaultHttpsPorts, panelVersion };
